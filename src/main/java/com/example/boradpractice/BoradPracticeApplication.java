f8a908cd1755f1836c603b50b808ece1d055a881
mini0-0 <nalala8200@naver.com>
2024-10-18 16:15:08 +0900

    #1- SpringBoot 시작

---
+++ src/main/java/com/example/boradpractice/BoradPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.boradpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoradPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoradPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boradpractice/BoradPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boradpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoradPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
