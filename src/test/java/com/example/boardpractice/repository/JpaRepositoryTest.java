bfc876fc39db74b75df3bca235fdfa327c1c1e6c
mini0-0 <nalala8200@naver.com>
2024-10-25 22:34:30 +0900

    # 7.test수정

--- src/test/java/com/example/boardpractice/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/boardpractice/repository/JpaRepositoryTest.java
@@ -8,13 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-@DisplayName("JPA 연결 테스트")
+@ActiveProfiles("testdb")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DisplayName("JPA 연결 테스트")
 @Import(JpaConfig.class)
 @DataJpaTest
 class JpaRepositoryTest {
