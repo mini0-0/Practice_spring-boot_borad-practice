5f5d5cb0aa348e46dc6938fdc7e0342a60e3dab4
mini0-0 <nalala8200@naver.com>
2024-12-16 17:41:28 +0900

    # 9 - data.sql 수정 data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- 테스트 계정
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅. 개선하는 것이 좋을 지 고민해 보자.
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
-    ('rose', '1234', 'Rose', 'Rose@mail.com', 'I am Rose.', now(), 'rose', now(), 'rose')
+    ('rose', '1234', 'Rose', 'rose@mail.com', 'I am Rose.', now(), 'rose', now(), 'rose')
 ;
 
 -- 123 게시글
