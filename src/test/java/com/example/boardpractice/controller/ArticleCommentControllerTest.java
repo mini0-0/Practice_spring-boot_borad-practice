56e50841ecf197a20ab4d431e3310e0eb7e5f0fc
mini0-0 <nalala8200@naver.com>
2025-01-08 16:42:30 +0900

    # 30 - 댓글 컨드롤러 테스트 업데이트: 대댓글 케이스 추가 대댓글이 포함됨 댓글 저장 api의 입출력 테스트 추가

--- src/test/java/com/example/boardpractice/controller/ArticleCommentControllerTest.java
+++ src/test/java/com/example/boardpractice/controller/ArticleCommentControllerTest.java
@@ -1,6 +1,5 @@
 package com.example.boardpractice.controller;
 
-import com.example.boardpractice.config.SecurityConfig;
 import com.example.boardpractice.config.TestSecurityConfig;
 import com.example.boardpractice.dto.ArticleCommentDto;
 import com.example.boardpractice.dto.request.ArticleCommentRequest;
@@ -90,6 +89,29 @@ class ArticleCommentControllerTest {
                 .andExpect(view().name("redirect:/articles/" + articleId))
                 .andExpect(redirectedUrl("/articles/" + articleId));
         then(articleCommentService).should().deleteArticleComment(articleCommentId, userId);
+    }
+
+    @WithUserDetails(value = "roseTest", setupBefore = TestExecutionEvent.TEST_EXECUTION)
+    @DisplayName("[view][POST] 대댓글 등록 - 정상 호출")
+    @Test
+    void givenArticleCommentInfoWithParentCommentId_whenRequesting_thenSavesNewChildComment() throws Exception {
+        // Given
+        long articleId = 1L;
+        ArticleCommentRequest request = ArticleCommentRequest.of(articleId, 1L, "test comment");
+        willDoNothing().given(articleCommentService).saveArticleComment(any(ArticleCommentDto.class));
+
+        // When & Then
+        mvc.perform(
+                post("/comments/new")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content(formDataEncoder.encode(request))
+                        .with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/articles/" + articleId))
+                .andExpect(redirectedUrl("/articles/" + articleId));
+
+        then(articleCommentService).should().saveArticleComment(any(ArticleCommentDto.class));
     }
 
 }
